ec3d3c6829403ef16ca778e804b447477770c8cb
connorshea <connor.james.shea@gmail.com>
2014-12-20 19:14:57 -0700

    Doesn't pointlessly run the JavaScript on mobile

--- js/main.js
+++ js/main.js
@@ -2,34 +2,42 @@
 ---
 
 $(function() {
-  // An array listing all the games which the sidebar can choose from.
-  var games = [60,220,440,620,730,228960,241600,251470];
+  
+  var windowwidth = $(window).width();
 
-  // Initializes the variable "i".
-  var i;
+  // If the window width is that of the "laptop" value established by Jekyll, the 
+  // following JavaScript should be executed, otherwise it should be ignored.
+  if (windowwidth > 800) {
 
-  // "For loop" that loops three times.
-  for (i = 1; i <= 3; i++) {
+    // An array listing all the games which the sidebar can choose from.
+    var games = [60,220,440,620,730,228960,241600,251470];
 
-    (function(index) {
+    // Initializes the variable "i".
+    var i;
 
-      // Chooses a random whole number between 0 and the number of games listed in the above array.
-      // Then it sets chosenGame equivalent to "games[x]" where x is the randomly generated number.
-      // "games[x]" is used to represent the value in the array located at "x" relative to the beginning of the array.
-      // So now chosenGame is a variable representing a randomly chosen game from the above array.
-      var randomNumber = Math.floor(Math.random() * games.length);
-      var chosenGame = games[randomNumber];
+    // "For loop" that loops three times.
+    for (i = 1; i <= 3; i++) {
 
-      // Sets variables for the game's Steam URL and Steam banner image.
-      var chosenGameURL = 'http://store.steampowered.com/app/' + chosenGame + '/';
-      var chosenGameImage = 'http://cdn.akamai.steamstatic.com/steam/apps/' + chosenGame + '/header.jpg';
+      (function(index) {
 
-      // Adds the Chosen Game's image and URL to the respective HTML elements.
-      $('#sidebar-game-' + i).attr('href',chosenGameURL);
-      $('#sidebar-game-image-' + i).attr('src',chosenGameImage);
+        // Chooses a random whole number between 0 and the number of games listed in the above array.
+        // Then it sets chosenGame equivalent to "games[x]" where x is the randomly generated number.
+        // "games[x]" is used to represent the value in the array located at "x" relative to the beginning of the array.
+        // So now chosenGame is a variable representing a randomly chosen game from the above array.
+        var randomNumber = Math.floor(Math.random() * games.length);
+        var chosenGame = games[randomNumber];
 
-      // Removes the game we used above from the array so we'll never have duplicates.
-      games.splice(randomNumber, 1);
-    })(i);
+        // Sets variables for the game's Steam URL and Steam banner image.
+        var chosenGameURL = 'http://store.steampowered.com/app/' + chosenGame + '/';
+        var chosenGameImage = 'http://cdn.akamai.steamstatic.com/steam/apps/' + chosenGame + '/header.jpg';
+
+        // Adds the Chosen Game's image and URL to the respective HTML elements.
+        $('#sidebar-game-' + i).attr('href',chosenGameURL);
+        $('#sidebar-game-image-' + i).attr('src',chosenGameImage);
+
+        // Removes the game we used above from the array so we'll never have duplicates.
+        games.splice(randomNumber, 1);
+      })(i);
+    }
   }
 });
