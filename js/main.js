af4b4c3fdedb9aa00ad1d223459859a50bf6f646
connorshea <connor.james.shea@gmail.com>
2014-12-20 18:56:19 -0700

    Sidebar now works, and with no duplicates!

--- js/main.js
+++ js/main.js
@@ -2,29 +2,34 @@
 ---
 
 $(function() {
-  console.log('Test1');
+  // An array listing all the games which the sidebar can choose from.
+  var games = [60,220,440,620,730,228960,241600,251470];
 
-  // An array listing all the games which the sidebar can choose from
-  var games = [220,730,228960,241600,251470];
+  // Initializes the variable "i".
+  var i;
 
-  console.log(games[1]);
+  // "For loop" that loops three times.
+  for (i = 1; i <= 3; i++) {
 
+    (function(index) {
+
+      // Chooses a random whole number between 0 and the number of games listed in the above array.
+      // Then it sets chosenGame equivalent to "games[x]" where x is the randomly generated number.
+      // "games[x]" is used to represent the value in the array located at "x" relative to the beginning of the array.
+      // So now chosenGame is a variable representing a randomly chosen game from the above array.
+      var randomNumber = Math.floor(Math.random() * games.length);
+      var chosenGame = games[randomNumber];
+
+      // Sets variables for the game's Steam URL and Steam banner image.
+      var chosenGameURL = 'http://store.steampowered.com/app/' + chosenGame + '/';
+      var chosenGameImage = 'http://cdn.akamai.steamstatic.com/steam/apps/' + chosenGame + '/header.jpg';
+
+      // Adds the Chosen Game's image and URL to the respective HTML elements.
+      $('#sidebar-game-' + i).attr('href',chosenGameURL);
+      $('#sidebar-game-image-' + i).attr('src',chosenGameImage);
+
+      // Removes the game we used above from the array so we'll never have duplicates.
+      games.splice(randomNumber, 1);
+    })(i);
+  }
 });
-
-// http://cdn.akamai.steamstatic.com/steam/apps/000000/header.jpg
-// http://store.steampowered.com/app/000000/
-
-// TowerFall Ascension
-// 251470
-
-// Rogue Legacy
-// 241600
-
-// Skulls of the Shogun
-// 228960
-
-// Half-Life 2
-// 220
-
-// Counter-Strike: Global Offensive
-// 730
