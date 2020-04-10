8d88c2adaab32e540b60c54ce761617326939b12
Ethan Lee <flibitijibibo@gmail.com>
2020-04-10 11:09:15 -0400

    Unrailed! now uses FNA

--- js/main.js
+++ js/main.js
@@ -82,6 +82,7 @@ $(function() {
       561740, // MidBoss
       602080, // Solaroids: Prologue
       701040, // CometStriker
+      1016920, // Unrailed!
       1258040, // Super Bernie World
     ];
 
