3acb70c8736cddc3166d00fefd353ce1a526bc70
Ethan Lee <flibitijibibo@gmail.com>
2022-06-10 08:56:16 -0400

    Blossom Tales 2 uses FNA!

--- js/main.js
+++ js/main.js
@@ -104,6 +104,7 @@ $(function() {
       1361510, // TMNT: Shredder's Revenge
       1503430, // Groov
       1637730, // Crystal Project
+      1747830, // Blossom Tales 2
       1765980, // Miasma 2: Freedom Uprising
       1771340, // Miasma: Citizens of Free Thought
       1800730, // I MAED A GAM3 W1TH Z0MB1ES 1N IT!!!1
